923bc2f155ab624295934597248ba0d9a0eda3d8
pecabum <pst_petrov@abv.bg>
2015-01-30 16:47:01 +0200

    added library fortune.js

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 var express = require('express');
-
+var fortune = require('./lib/fortune.js');
 var app = express();
 // set up handlebars view engine
 
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
-
-var fortunes = [
-"Conquer your fears or they will conquer you.",
-"Rivers need springs.",
-"Do not fear what you don't know.",
-"You will have a pleasant surprise.",
-"Whenever possible, keep it simple.",
-];
 
 //Set handlebar library
 app.engine('handlebars', handlebars.engine);
@@ -24,8 +16,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/about', function(req, res){
-    var randomFortune =fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 404 catch-all handler (middleware)
